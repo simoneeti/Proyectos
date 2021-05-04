8864b58630609f8c51faeb58a3ed55506dc160a9
simoneeti <vintrob.simon@gmail.com>
2021-05-04 10:13:25 -0300

    deleted useless files

--- Vacuna/1.js
+++
@@ -1,9 +0,0 @@
-var jsdom = require("jsdom");
-
-var url = 'https://www.buenosaires.gob.ar/coronavirus/vacunacion-covid-19/personal-de-salud';
-
-jsdom.JSDOM.fromURL(url)
-
-    .then(function (dom) {
-        console.log(dom.window.document.getElementById('fondoColor-1').textContent.trim())
-    });
